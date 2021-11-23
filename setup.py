35248d7b1ecfbb0719aab3f424212d4c0fec5116
Iván de Prado <ivanprado@users.noreply.github.com>
2021-11-23 10:48:01 +0100

    Fix py36

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
     install_requires=[
         "tldextract",
         "dataclasses>=0.6; python_version < '3.7'",
+        "types-dataclasses>=0.6.1; python_version < '3.7'",
     ],
     classifiers=[
         "Development Status :: 3 - Alpha",

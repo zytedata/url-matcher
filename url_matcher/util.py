02e724fdd088c79f16ecb31a093c3ccb25f1ee2a
Iván de Prado <ivanprado@users.noreply.github.com>
2021-11-29 10:41:27 +0100

    Type annotations

--- url_matcher/util.py
+++ url_matcher/util.py
@@ -5,7 +5,7 @@ from tldextract import tldextract
 
 
 @lru_cache(100)
-def get_domain(url) -> str:
+def get_domain(url: str) -> str:
     """
     Return the domain without any subdomain
 
@@ -21,5 +21,5 @@ def get_domain(url) -> str:
     return ".".join(el for el in tldextract.extract(url)[-2:] if el)
 
 
-def is_absolute(url) -> bool:
+def is_absolute(url: str) -> bool:
     return bool(urlparse(url).netloc)
